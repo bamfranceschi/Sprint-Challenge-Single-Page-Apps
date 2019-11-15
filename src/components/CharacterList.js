87c341335f6adb6897836ccfcf8bfbe6dbd0c9c2
bamfranceschi <anna.franceschi@gmail.com>
2019-11-15 11:50:48 -0600

    all but search form complete

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,51 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import styled from "styled-components";
+import SearchForm from "./SearchForm";
+
+const CharDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top: 10%;
+  margin-left: 4%;
+  margin-right: 4%;
+`;
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        setCharacter(response.data.results);
+      })
+      .catch(error => {
+        console.log("There was an error fetching the data", error);
+      });
+  }, [setCharacter]);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <div>
+        <SearchForm />
+      </div>
+
+      <CharDiv>
+        {character.map(person => (
+          <CharacterCard
+            key={person.id}
+            name={person.name}
+            status={person.status}
+            species={person.species}
+            type={person.type}
+            gender={person.gender}
+          />
+        ))}
+      </CharDiv>
     </section>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function SearchForm() {
- 
+export default function SearchForm(props) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const results = character.filter(person =>
+      person.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <label htmlFor="search">Search:</label>
+        <input name="search" placeholder="search" onChange={handleChange} />
+      </form>
+
+      <div>
+        <ul>
+          {searchResults.map(props.person => (
+            <li key={props.person}>{props.person}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
